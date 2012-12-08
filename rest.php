c9757b69f6bb4f6f87549ecd5084acfadc82404e
Ebot Tabi <ebot.tabi@gmail.com>
2012-12-08 14:15:05 +0100

    updates on rest.php

--- rest.php
+++ rest.php
@@ -3,7 +3,7 @@
  * RESTful Client based on PHP CURL
  * 
  * @author      Ebot Tabi <ebot.tabi@gmail.com>
- * @link		https://github.com/vc4africa/restful_php_client.git
+ * @link		https://develoers.vc4africa.biz
  * @version 	1.0.0
  * @date		05.12.2012
  */
